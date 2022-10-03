937f322238553f14faac01ba10355ee33f1e05f3
RonaldoCMS <ronaldocms@outlook.it>
2022-10-03 13:51:26 +0200

    Initial Commit

---
+++ petshop/petshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  petshopApp.swift
+//  petshop
+//
+//  Created by Fabio Danubbio on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct petshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
